a6a888530e35ee3f24f42d21cb642a4a59b20a79
Kunalpatil009 <009kunalpatil@gmail.com>
2022-06-28 20:12:47 +0530

    JLP_Prb5_solution

---
+++ Coupon_Num.java
@@ -0,0 +1,23 @@
+package javalogic;
+
+public class Coupon_Num {
+    public static void main(String[] args) {
+
+        char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789".toCharArray();
+        int max = 10000000;
+        int random = (int) (Math.random() * max);
+        StringBuffer sc = new StringBuffer();
+
+
+        while (random > 0) {
+
+            sc.append(chars[random % chars.length]);
+            random /= chars.length;
+        }
+
+        String couponCode = sc.toString();
+        System.out.println("Coupon Code: " + couponCode);
+    }
+}
+
+
