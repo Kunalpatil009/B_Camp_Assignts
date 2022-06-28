21166396967ab751113de49cec0d125f2b351a97
Kunalpatil009 <009kunalpatil@gmail.com>
2022-06-28 20:12:01 +0530

    JLP_Prb4_solution

---
+++ Reverse_Number.java
@@ -0,0 +1,21 @@
+package javalogic;
+import java.util.Scanner;
+
+public class Reverse_Number {
+    public static void main(String[] args) {
+
+
+        int number;
+        int Reverse = 0;
+        Scanner num = new Scanner(System.in);
+        System.out.println("Enter the Number :");
+        number= num.nextInt();
+        while(number != 0)
+        {
+            int remainder = number % 10;
+            Reverse = Reverse * 10 + remainder;
+            number = number/10;
+        }
+        System.out.println("The reverse of the given number is: " + Reverse);
+    }
+}
