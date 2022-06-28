e1bad22aece904db32ee4f5cab714bb0cc6625bb
Kunalpatil009 <009kunalpatil@gmail.com>
2022-06-28 20:07:19 +0530

    JLP_Prb2_solution

---
+++ Perfect_Number.java
@@ -0,0 +1,27 @@
+package javalogic;
+import java.util.Scanner;
+
+public class Perfect_Number {
+    public static void main(String[] args) {
+
+        int a;
+        int num;
+        int sum = 0;
+        Scanner user = new Scanner(System.in);
+        System.out.println("Enter The Number To Check a Perfect Number: ");
+        num = user.nextInt();
+
+        for (a = 1; a < num; a++) {
+            if (num % a == 0) ;
+            sum += a;
+        }
+        if (sum == num) {
+            System.out.print( "Entered Number "+ num );
+            System.out.print(" is Perfect Number");
+        } else {
+            System.out.print("Entered Number "+ num );
+            System.out.print(" is not Perfect Number");
+        }
+
+    }
+}
